2f6b95188022d736703c7e2dd8c78fe5873080aa
Eugene Bekker <ebekker@users.noreply.github.com>
2018-07-02 08:50:55 -0400

    exploring script-based PWSH module for ACMESharpCore

---
+++ ACMESharpCore/ACMESharpCore.psm1
@@ -0,0 +1,30 @@
+﻿
+$ErrorActionPreference = 'Stop'
+
+function Get-ACMETermsOfService {
+<#
+.DESCRIPTION
+Retrieves the Terms of Service for an ACME CA.
+#>
+
+}
+
+function New-ACMEAccount {
+<#
+.DESCRIPTION
+Creates a new Account with an ACME CA.
+
+.PARAMETER Emails
+One or more email addresses to associate as a contact with the ACME Account.
+
+.PARAMETER AcceptToS
+Switch to indicate acceptances of ACME CA's Terms of Service.
+#>
+
+param(
+	[string[]]$Emails,
+	[switch]$AcceptToS
+)
+
+}
+
