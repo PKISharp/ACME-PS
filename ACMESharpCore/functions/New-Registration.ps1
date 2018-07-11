0bf81077055069ce8c1ad922135ce019e174b024
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-07-11 18:01:04 +0200

    Ignore Local-Storeage for now

--- ACMESharpCore/functions/New-Registration.ps1
+++ ACMESharpCore/functions/New-Registration.ps1
@@ -1,14 +1,9 @@
 function New-Registration {
     [CmdletBinding(DefaultParameterSetName="Store", SupportsShouldProcess=$true)]
     param(
-        [Parameter(ParameterSetName="Store")]
-        [ValidateNotNullOrEmpty]
-        [string]
-        $ACMEStoreDir = ".",
-
         [Parameter(Mandatory=$true,ParameterSetName="Direct")]
         [ValidateNotNullOrEmpty]
-        [ACMESharp.Crypto.JOSE.JwsExport] $JwsExport,
+        [ACMESharp.Crypto.JOSE.JwsAlgorithm] $JwsAlgorithm,
 
         [Parameter(Mandatory=$true,ParameterSetName="Direct")]
         [ValidateNotNullOrEmpty]
@@ -23,17 +18,11 @@ function New-Registration {
         $EmailAddresses
     )
 
-    if($PSCmdlet.ParameterSetName -eq "Store") {
-        $localStore = [LocalStore]::Load($ACMEStoreDir);
-        $ACMENewAccountUrl = $localStore.Directory.NewAccount;
-        $JwsExport = $localStore.AccountKey;
-    }
-
     $payload = [ACMESharp.Protocol.Messages.CreateAccountRequest]::new();
     $payload.TermsOfServiceAgreed = $AcceptTOS;
     $payload.Contacts = $EmailAddresses | ForEach-Object { "mail:$_" }
 
-    $request = Create-SignedMessage -Payload $payload -JwsExport $JwsExport
+    $request = New-SignedMessage -Payload $payload -JwsAlgorithm $JwsAlgorithm
 
     if($PSCmdlet.ShouldProcess("New-Registration", "Sending registration to ACME Server $ACMENewAccountUrl")) {
         Invoke-WebRequest $ACMENewAccountUrl -Method POST -Body $request
