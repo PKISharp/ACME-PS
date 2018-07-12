15859e326093215d88dd4e765ff3f5324f030b6a
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-07-12 12:06:09 +0200

    New-Nonce Cmdlet

---
+++ ACMESharpCore/functions/New-Nonce.ps1
@@ -0,0 +1,22 @@
+function New-Nonce {
+    <#
+        .SYNOPSIS
+            Gets a new nonce from the uri
+        .DESCRIPTION
+            Issues a web request to receive a new nonce from the given uri
+
+        .EXAMPLE
+            PS> New-Nonce -Uri "https://acme-staging-v02.api.letsencrypt.org/acme/new-nonce"
+    #>
+    [CmdletBinding()]
+    [OutputType("String")]
+    param(
+        # The Uri to send the new-nonce request to
+        [Parameter(Mandatory = $true, Position = 0)]
+        [uri]
+        $Uri
+    )
+
+    $response = Invoke-WebRequest $Uri -Method Head
+    return $response.Headers["replay-nonce"];
+}
