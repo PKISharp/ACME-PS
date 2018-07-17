62bb179a92b21be8bc1fab9f31e59631c3f6bdac
Thomas Glatzer <glatzert@outlook.de>
2018-07-17 11:28:07 +0200

    REmove Headers; Enable Auto-Nonce

--- ACMESharpCore/internal/functions/Invoke-AcmeWebRequest.ps1
+++ ACMESharpCore/internal/functions/Invoke-AcmeWebRequest.ps1
@@ -19,21 +19,11 @@ function Invoke-ACMEWebRequest {
         [Parameter(Mandatory = $true)]
         [ValidateSet("GET", "POST", "HEAD")]
         [string]
-        $Method,
-
-        # Parameter help description
-        [Parameter()]
-        [ValidateNotNull()]
-        [hashtable]
-        $Headers
+        $Method
     )
 
     $httpRequest = [System.Net.Http.HttpRequestMessage]::new($Method, $Uri);
     Write-Verbose "Sending HttpRequest ($Method) to $Uri";
-    
-    foreach($header in $Headers) {
-        $httpRequest.Headers.Add($header.Key, $header.Value)
-    }
 
     if($Method -in @("POST", "PUT")) {
         $httpRequest.Content = [System.Net.Http.StringContent]::new($JsonBody, [System.Text.Encoding]::UTF8);
@@ -48,13 +38,19 @@ function Invoke-ACMEWebRequest {
     $httpResponse = $httpClient.SendAsync($httpRequest).GetAwaiter().GetResult();
     $response = $httpResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
-    if($httpResponse.IsSuccessStatusCode) {
-        return [ACMEResponse]::new($httpResponse, $response);
-    }
-
     if($httpResponse.Content.Headers.ContentType -eq "application/problem+json") {
         Write-Error "Server returned Problem: $response"
-        return [ACMEResponse]::new($httpResponse, $response);
+    }
+
+    if($httpRequest.StatusCode -lt 500) {
+        $result = [AcmeHttpResponse]::new($httpResponse, $response);
+
+        if($Script:AutoNonce) {
+            Write-Verbose "Setting Module Nonce to $($result.NextNonce)";
+            $Script:Nonce = $result.NextNonce;
+        }
+
+        return $result;
     }
 
     Write-Error "Unexpected response from server: $($httpResponse.StatusCode), with content:`n$response"
