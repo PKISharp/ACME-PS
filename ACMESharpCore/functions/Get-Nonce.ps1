98547605f4096959605b20b1c2b3558ad0b4ecc6
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-08-03 16:53:41 +0200

    Get-Nonce

--- ACMESharpCore/functions/Get-Nonce.ps1
+++ ACMESharpCore/functions/Get-Nonce.ps1
@@ -1,9 +1,14 @@
 function Get-Nonce {
     param()
 
-    Write-Warning "If you use this nonce outside of the AcmeSharpCore-Module, "+
-        "it might be neccessary to call New-Nonce to reinitialize the automatic nonce handling."+
-        "calling any other module function, which contacts the acme-server will also suffice."
+    if($script:AutoNonce) {
+        Write-Warning "If you use this nonce outside of the AcmeSharpCore-Module, "+
+            "it might be neccessary to call New-Nonce to reinitialize the automatic nonce handling."+
+            "calling any other module function, which contacts the acme-server will also suffice.";
+    } else {
+        Write-Error "Using this function is only possible, if Get-ServiceDirectory was called with -EnableModuleNonceHandling, "+
+            "if you need to access the nonce, refer to the AcmeHttpResponse-Object in the Result of your functions or call New-Nonce";
+    }
 
     return $Script:Nonce;
 }
