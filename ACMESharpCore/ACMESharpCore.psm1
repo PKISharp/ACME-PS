2e8d42a69c453d671ac7bed1005908b7967bfe13
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-08-03 14:59:17 +0200

    Try another module layout

--- ACMESharpCore/ACMESharpCore.psm1
+++ ACMESharpCore/ACMESharpCore.psm1
@@ -19,44 +19,61 @@ function Import-ModuleFile {
     if ($script:doDotSource) { . $Path }
     else { $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($Path))), $null, $null) }
 }
- 
-$script:PSModuleRoot = $PSScriptRoot
 
-if ((Test-Path -Path "$script:PSModuleRoot\internal\classes")) {
-    $classPath = "$script:PSModuleRoot\internal\classes";
-    $classes = @(
-        "AcmeHttpResponse",
-        "AcmeDirectory",
-        "AcmeAccount",
-        "AcmeIdentifier",
-        "AcmeChallenge",
-        "AcmeOrder",
-        "AcmeAuthorization"
+function Merge-ModuleFiles {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ParameterSetName="Path")]
+        [ValidateNotNullOrEmpty()]
+        [string] $Path,
+
+        [Parameter(Mandatory = $true, ParameterSetName="Files")]
+        [ValidateNotNullOrEmpty()]
+        [string[]] $Files,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $OutFile
     )
 
-    $classMergeFile = "$classPath\..\AllClasses.ps1";
-    if(Test-Path $classMergeFile) {
-        Clear-Content "$classPath\..\AllClasses.ps1"
+    if(Test-Path $OutFile) {
+        Remove-Item $OutFile
+        New-Item $OutFile
     }
 
-    $classes | ForEach-Object { Get-Content "$classPath\$_.ps1" } | Set-Content $classMergeFile
-
-    #TODO: This is a workaround for loading the functions. Needed it, because using a class inside the ctor of another class was not possible.
-    . Import-ModuleFile "$classPath\..\AllClasses.ps1";
-
-    # All internal functions privately available within the toolset
-    foreach ($function in (Get-ChildItem "$script:PSModuleRoot\internal\functions\*.ps1")) {
-        . Import-ModuleFile $function.FullName;
+    if($PSCmdlet.ParameterSetName -eq "Path") {
+        $Files = Get-Item -Path $Path
     }
 
-    # All exported functions
-    foreach ($function in (Get-ChildItem "$script:PSModuleRoot\functions\*.ps1")) {
-        . Import-ModuleFile $function.FullName;
-    }
+    $Files | ForEach-Object { Get-Content $_ } | Set-Content $OutFile
 }
-else {
-    # This is created by the build script and will improve module loading time
-    . "$script:PSModuleRoot\FullModule.ps1"
-}
+
+
+$script:PSModuleRoot = $PSScriptRoot
+
+$classPath = "$script:PSModuleRoot\internal\classes";
+$classes = @(
+    "AcmeHttpResponse",
+    "AcmeDirectory",
+    "AcmeAccount",
+    "AcmeIdentifier",
+    "AcmeChallenge",
+    "AcmeOrder",
+    "AcmeAuthorization"
+)
+
+$classMergeFile = "$classPath\..\AllClasses.ps1";
+$internalFunctions = "$script:PSModuleRoot\internal\AllFunctions.ps1"
+$exportedFunctions = "$script:PSModuleRoot\AllFunctions.ps1"
+
+Merge-ModuleFiles -Files @($classes | ForEach-Object { "$classPath\$_.ps1" }) -OutFile $classMergeFile
+Merge-ModuleFiles -Path "$script:PSModuleRoot\internal\functions\*.ps1" -OutFile $internalFunctions
+Merge-ModuleFiles -Path "$script:PSModuleRoot\functions\*.ps1" -OutFile $exportedFunctions
+
+$classes | ForEach-Object { Get-Content "$classPath\$_.ps1" } | Set-Content $classMergeFile
+
+. Import-ModuleFile $classMergeFile;
+. Import-ModuleFile $internalFunctions;
+. Import-ModuleFile $externalFunctions;
 
 $Script:AutoNonce = $true
