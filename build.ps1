dad94697b03694fa26664b36407c38d9eb06f4c8
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-08-14 16:27:41 +0200

    Adjusting some build-script flaws

--- build.ps1
+++ build.ps1
@@ -5,13 +5,7 @@ param(
     [string] $ModuleOutPath = "./publish",
 
     [Parameter()]
-    [Switch] $PublishModule,
-
-    [Parameter()]
-    [Switch] $SignModule,
-
-    [Parameter()]
-    [Switch] $SkipDependencies
+    [Switch] $SignModule
 )
 
 $ErrorActionPreference = 'Stop';
@@ -22,16 +16,20 @@ $ModuleOutPath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScri
 
 <# Clean Publish folder #>
 if(Test-Path $ModuleOutPath) {
-    Write-Information "Deleting $ModuleOutPath/*";
+    Write-Information "Delete old files from $ModuleOutPath";
     Get-ChildItem "$ModuleOutPath/*" -Recurse | Remove-Item -Force -Recurse | Out-Null
 } else {
+    Write-Information "Creating $ModuelOutPath";
     New-Item $ModuleOutPath -ItemType Directory
 }
 
 <# Publish the module #>
-Import-Module "$PSScriptRoot/ACMESharpCore" -Force -ErrorAction 'Stop' # This will create the All* files.
+Import-Module "$PSScriptRoot/ACMESharpCore" -Force -ErrorAction 'Stop' -Verbose:$false # This will create the All* files.
 
-Copy-Item -Path "$ModuleSourcePath/ACMESharpCore.psd1" -Destination "$ModuleOutPath/ACMESharpCore.psd1" -Force;
+Write-Information "Copying $ModuleSourcePath/ACMESharpCore.psd1"
+Copy-Item -Path "$ModuleSourcePath/ACMESharpCore.psd1" -Destination "$ModuleOutPath/" -Force;
+
+Write-Information "Copying $ModuleSourcePath/TypeDefinitions.psd1"
 Copy-Item -Path "$ModuleSourcePath/TypeDefinitions.ps1" -Destination "$ModuleOutPath/" -Force;
 
 $ModuleFiles = @(
@@ -40,11 +38,15 @@ $ModuleFiles = @(
     "AllFunctions.ps1"
 )
 
+Write-Information "Merging Module content files $([string]::Join(", ", $ModuleFiles))"
 $ModuleFiles | ForEach-Object { Get-Content "$ModuleSourcePath/$_" } | Set-Content "$ModuleOutPath/ACMESharpCore.psm1";
 
 if($SignModule) {
-    $files = "$ModuleOutPath/ACMESharpCore.ps*"
+    Write-Information "Signing all *.ps* files"
+    $files = "$ModuleOutPath/*.ps*"
     $cert = Get-Item Cert:\CurrentUser\My\017E67F53FCB161D63E7881F1F96A8452859200D
 
-    Set-AuthenticodeSignature -FilePath $files -Certificate $cert
+    Set-AuthenticodeSignature -FilePath $files -Certificate $cert | Out-Null
 }
+
+Write-Information "Finished";
