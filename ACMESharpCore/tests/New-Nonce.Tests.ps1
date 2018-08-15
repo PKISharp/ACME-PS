b2a7d07ff90fb634ae82b3540c98839f41c70971
Thomas Glatzer <glatzert@outlook.de>
2018-08-15 23:56:39 +0200

    UnitTesting New-Nonce

---
+++ ACMESharpCore/tests/New-Nonce.Tests.ps1
@@ -0,0 +1,31 @@
+InModuleScope ACMESharpCore {
+    Describe "UnitTesting New-Nonce" -Tag "UnitTest" {
+        $state = Get-State -Path $PSScriptRoot\states\simple
+        $state.AutoSave = $false;
+
+        Context "The New-Nonce function" {
+            Mock Invoke-AcmeWebRequest { 
+                $mockResult = [AcmeHttpResponse]::new();
+                $mockResult.NextNonce = "MyNewNonce";
+                return  $mockResult;
+            } -Verifiable -ParameterFilter { 
+                $Url -eq $state.GetServiceDirectory().NewNonce -and 
+                $Method -eq "HEAD"
+            }
+
+            $nonce = New-Nonce $state -PassThrough;
+
+            It 'called the ACME service' {
+                Assert-VerifiableMock
+            }
+
+            It 'passes through the nonce' {
+                $nonce | Should -Be "MyNewNonce";
+            }
+
+            It 'sets the nonce in $state' {
+                $state.GetNonce() | Should -Be "MyNewNonce";
+            }
+        }
+    }
+}
