bd4baba49f8f42c46793e860cee955dad7b238ab
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-08-20 17:50:26 +0200

    Updating the order could not update the state-instance

--- ACMESharpCore/functions/Order/Update-Order.ps1
+++ ACMESharpCore/functions/Order/Update-Order.ps1
@@ -4,16 +4,28 @@ function Update-Order {
             Updates an order from acme service
 
         .DESCRIPTION
-            Uses the order to fetch an update from the acme service.
+            Updates the given order instance by querying the acme service.
+            The result will be used to update the order stored in the state object
 
+        .PARAMETER State
+            State instance containing service directory, account key, account and nonce.
 
         .PARAMETER Order
             The order to be updated.
+
+        .PARAMETER PassThrough
+            If present, the updated order will be written to the output.
     #>
     [CmdletBinding()]
     [OutputType("AcmeOrder")]
     param(
         [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNull()]
+        [ValidateScript({$_.Validate()})]
+        [AcmeState]
+        $State,
+
+        [Parameter(Mandatory = $true, Position = 1, ValueFromPipeline = $true)]
         [ValidateNotNullOrEmpty()]
         [AcmeOrder]
         $Order,
