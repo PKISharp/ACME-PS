1c38ab525d2e677962d31a46b8b882e6c3cb8285
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-08-20 17:54:24 +0200

    Get-Challange also initializes the challange data

--- ACMESharpCore/functions/Challenge/Get-Challenge.ps1
+++ ACMESharpCore/functions/Challenge/Get-Challenge.ps1
@@ -1,15 +1,26 @@
 function Get-Challenge {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNull()]
+        [ValidateScript({$_.Validate()})]
+        [AcmeState]
+        $State,
+
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
         [ValidateNotNull()]
         [AcmeAuthorization] $Authorization,
 
-        [Parameter(Mandatory = $true, Position = 1)]
+        [Parameter(Mandatory = $true, Position = 2)]
         [string] $Type
     )
 
     process {
-        return $Authorization.Challenges | Where-Object { $_.Type -eq $Type } | Select-Object -First 1
+        $challange = $Authorization.Challenges | Where-Object { $_.Type -eq $Type } | Select-Object -First 1
+        if(-not $challange.Data) {
+            $challange | Initialize-Challange $State 
+        }
+
+        return $challange;
     }
 }
