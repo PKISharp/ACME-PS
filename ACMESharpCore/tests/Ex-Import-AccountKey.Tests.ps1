6a0aece80e8a26043df69118a1eb2628da9f9eb1
Thomas Glatzer <glatzert@outlook.de>
2018-09-02 20:03:41 +0200

    Roundtripping Keys Tests

--- ACMESharpCore/tests/Ex-Import-AccountKey.Tests.ps1
+++ ACMESharpCore/tests/Ex-Import-AccountKey.Tests.ps1
@@ -8,7 +8,30 @@ InModuleScope ACMESharpCore {
             $accountKey | Export-AccountKey -Path $tempFile
 
             $importedKey = Import-AccountKey -State $state -Path $tempFile
-         
+            
+            $orgiginalKeySignature = $accountKey.Sign("Test");
+            $importedKeySignature = $importedKey.Sign("Test");
+            
+            $importedKeySignature | Should -be $orgiginalKeySignature;
+
+            Remove-Item $tempFile;
+        }
+
+        Context "Roundtripping JSON" {
+            $tempFile = [System.IO.Path]::GetTempFileName() + ".json";
+            $state = New-State;
+            
+            $accountKey = New-AccountKey;
+            $accountKey | Export-AccountKey -Path $tempFile
+
+            $importedKey = Import-AccountKey -State $state -Path $tempFile
+            
+            $orgiginalKeySignature = $accountKey.Sign("Test");
+            $importedKeySignature = $importedKey.Sign("Test");
+            
+            $importedKeySignature | Should -be $orgiginalKeySignature;
+
+            Remove-Item $tempFile;
         }
     }
 }
