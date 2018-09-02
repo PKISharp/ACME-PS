70adf74af61ceebf7df456d693ca9b7d2e568b22
Thomas Glatzer <glatzert@outlook.de>
2018-09-02 20:34:14 +0200

    SImplify test

--- ACMESharpCore/tests/Ex-Import-AccountKey.Tests.ps1
+++ ACMESharpCore/tests/Ex-Import-AccountKey.Tests.ps1
@@ -2,12 +2,11 @@ InModuleScope ACMESharpCore {
     Describe "UnitTesting Import-AccountKey and Export-AccountKey" -Tag "UnitTest" {
         Context "Roundtripping XML" {
             $tempFile = [System.IO.Path]::GetTempFileName() + ".xml";
-            $state = New-State;
             
             $accountKey = New-AccountKey;
             $accountKey | Export-AccountKey -Path $tempFile
 
-            $importedKey = Import-AccountKey -State $state -Path $tempFile
+            $importedKey = Import-AccountKey -Path $tempFile
             
             $orgiginalKeySignature = $accountKey.Sign("Test");
             $importedKeySignature = $importedKey.Sign("Test");
@@ -19,12 +18,11 @@ InModuleScope ACMESharpCore {
 
         Context "Roundtripping JSON" {
             $tempFile = [System.IO.Path]::GetTempFileName() + ".json";
-            $state = New-State;
-            
+
             $accountKey = New-AccountKey;
             $accountKey | Export-AccountKey -Path $tempFile
 
-            $importedKey = Import-AccountKey -State $state -Path $tempFile
+            $importedKey = Import-AccountKey -Path $tempFile
             
             $orgiginalKeySignature = $accountKey.Sign("Test");
             $importedKeySignature = $importedKey.Sign("Test");
