d9784167a7145f2086eee96a1dff3a2f3abc629e
Thomas Glatzer <glatzert@outlook.de>
2018-09-02 20:48:13 +0200

    More test output

--- ACMESharpCore/tests/Ex-Import-AccountKey.Tests.ps1
+++ ACMESharpCore/tests/Ex-Import-AccountKey.Tests.ps1
@@ -1,33 +1,53 @@
 InModuleScope ACMESharpCore {
     Describe "UnitTesting Import-AccountKey and Export-AccountKey" -Tag "UnitTest" {
-        It "Roundtripped XML File will correctly sign data" {
+        Context "Roundtripping XML" {
             $tempFile = [System.IO.Path]::GetTempFileName() + ".xml";
             
             $accountKey = New-AccountKey;
             $accountKey | Export-AccountKey -Path $tempFile
 
+            It 'Created the export file' {
+                Test-Path $tempFile | Should -Be $true
+            }
+
             $importedKey = Import-AccountKey -Path $tempFile
             
+            It 'Imported the key from the export' {
+                $importedKey | Should -not -be $null
+            }
+
             $orgiginalKeySignature = $accountKey.Sign("Test");
             $importedKeySignature = $importedKey.Sign("Test");
             
-            $importedKeySignature | Should -be $orgiginalKeySignature;
+            It 'signed a test word with the same result.' {
+                $importedKeySignature | Should -be $orgiginalKeySignature;
+            }
             
             Remove-Item $tempFile;
         }
 
-        It "Roundtripped JSON File will correctly sign data" {
+        Context "Roundtripping JSON" {
             $tempFile = [System.IO.Path]::GetTempFileName() + ".json";
 
             $accountKey = New-AccountKey;
             $accountKey | Export-AccountKey -Path $tempFile
 
+            It 'Created the export file' {
+                Test-Path $tempFile | Should -Be $true
+            }
+
             $importedKey = Import-AccountKey -Path $tempFile
             
+            It 'Imported the key from the export' {
+                $importedKey | Should -not -be $null
+            }
+
             $orgiginalKeySignature = $accountKey.Sign("Test");
             $importedKeySignature = $importedKey.Sign("Test");
             
-            $importedKeySignature | Should -be $orgiginalKeySignature;
+            It 'signed a test word with the same result.' {
+                $importedKeySignature | Should -be $orgiginalKeySignature;
+            }
 
             Remove-Item $tempFile;
         }
