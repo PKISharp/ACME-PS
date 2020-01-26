7538558372159ba66f998d30ed097b251003e083
Thomas Glatzer <glatzert@outlook.de>
2020-01-26 14:00:25 +0100

    Implement auto cert-key generation

--- ACME-PS/functions/Order/Complete-Order.ps1
+++ ACME-PS/functions/Order/Complete-Order.ps1
@@ -17,12 +17,22 @@ function Complete-Order {
         .PARAMETER CertificateKey
             The certificate key to be used to create the certificate signing request.
 
+        .PARAMETER SaveCertificateKey
+            If present, the certificate will be saved with the order object. Use this, if the certificate key
+            has not been exported, yet.
+
+        .PARAMETER GenerateCertificateKey
+            If present, the cmdlet will automatically create a certificate key and store it with the order object.
+
         .PARAMETER PassThru
             Forces the order to be returned to the pipeline.
 
 
         .EXAMPLE
             PS> Complete-Order -State $myState -Order $myOrder -CertificateKey $myCertKey
+        
+        .EXAMPLE
+            PS> Complete-Order -State $myState -Order $myOrder -GenerateCertificateKey
     #>
     [CmdletBinding(SupportsShouldProcess=$true)]
     param(
@@ -37,10 +47,18 @@ function Complete-Order {
         [AcmeOrder]
         $Order,
 
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, ParameterSetName="CustomKey")]
         [ValidateNotNull()]
         [ICertificateKey]
         $CertificateKey,
+
+        [Parameter(ParameterSetName="CustomKey")]
+        [switch]
+        $SaveCertificateKey,
+
+        [Parameter(ParameterSetName="AutoKey")]
+        [switch]
+        $GenerateCertificateKey,
 
         [Parameter()]
         [switch]
@@ -49,6 +67,15 @@ function Complete-Order {
 
     process {
         $ErrorActionPreference = 'Stop';
+
+        if($GenerateCertificateKey) {
+            $SaveCertificateKey = $true;
+            $CertificateKey = New-CertificateKey -SkipKeyExport -WarningAction 'SilentlyContinue'
+        }
+
+        if($SaveCertificateKey) {
+            $State.SetOrderCertificateKey($Order, $CertificateKey);
+        }
 
         $dnsNames = $Order.Identifiers | ForEach-Object { $_.Value }
         if($Order.CSROptions -and -not [string]::IsNullOrWhiteSpace($Order.CSROptions.DistinguishedName)) {
