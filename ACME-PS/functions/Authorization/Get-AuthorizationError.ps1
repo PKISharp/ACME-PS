768f8986aa8e8e31f76af6a772e324bf8a9422ea
Thomas Glatzer <glatzert@outlook.de>
2020-04-23 22:32:21 +0200

    implement get-authErrors

---
+++ ACME-PS/functions/Authorization/Get-AuthorizationError.ps1
@@ -0,0 +1,44 @@
+function Get-AuthorizationError {
+    <#
+        .SYNOPSIS
+            Fetches authorizations erros from acme service.
+
+        .DESCRIPTION
+            Fetches all authorization errors for an order.
+
+
+        .PARAMETER Order
+            The order, whoose authorizations errors will be fetched (needs to be in invalid state)
+
+        .PARAMETER State
+            The state object, that is used in this module, to provide easy access to the ACME service directory,
+            your account key, the associated account and the replay nonce.
+
+
+        .EXAMPLE
+            PS> Get-AuthorizationError $myOrder $myState
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNull()]
+        [ValidateScript({$_.AccountExists()})]
+        [AcmeState]
+        $State,
+
+        [Parameter(Mandatory = $true, Position = 1, ValueFromPipeline = $true, ParameterSetName = "FromOrder")]
+        [ValidateNotNull()]
+        [AcmeOrder]
+        $Order
+    )
+
+    process {
+        if ($Order.Status -ine "invalid") {
+            return;
+        }
+
+        $authorizations = $Order.AuthorizationUrls | ForEach-Object { Get-Authorization -Url $_ $State }
+        $invalidAuthorizations = $authorizations | Where-Object { $_.Status -ieq "invalid" };
+        $invalidAuthorizations | ForEach-Object { $_.Challenges | Where-Object { $_.Status -ieq "invalid" } }
+    }
+}
