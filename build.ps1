494ebf03745de554b3a6d8c673230b78d7acac25
Thomas Glatzer <glatzert@outlook.de>
2020-05-21 15:52:48 +0200

    Simplify build script (omit module loading)

--- build.ps1
+++ build.ps1
@@ -2,58 +2,114 @@
 param(
     [Parameter()]
     [ValidateNotNullOrEmpty()]
-    [string] $ModuleOutPath = "./publish/ACME-PS",
+    [string] $ModuleOutPath = "./build/ACME-PS",
 
     [Parameter()]
     [Switch] $SignModule
 )
 
-$ErrorActionPreference = 'Stop';
-$InformationPreference = 'Continue';
+begin {
+    function Merge-ContentInto {
+        param(
+            [Parameter(Mandatory = $true, ValueFromPipeline=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string] $File,
 
-$ModuleSourcePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "./ACME-PS"));
-$ModuleOutPath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $ModuleOutPath));
+            [Parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [string] $OutFile
+        )
 
-<# Clean Publish folder #>
-if(Test-Path $ModuleOutPath) {
-    Write-Information "Delete old files from $ModuleOutPath";
-    Get-ChildItem "$ModuleOutPath/*" -Recurse | Remove-Item -Force -Recurse | Out-Null
-} else {
-    Write-Information "Creating $ModuelOutPath";
-    New-Item $ModuleOutPath -ItemType Directory
+        process {
+            Get-Content $_ | Add-Content $OutFile
+        }
+    }
 }
 
-<# Publish the module #>
-Write-Information "Copying $ModuleSourcePath/ACME-PS.psd1"
-Copy-Item -Path "$ModuleSourcePath/ACME-PS.psd1" -Destination "$ModuleOutPath/" -Force;
+process {
+    $ErrorActionPreference = 'Stop';
+    $InformationPreference = 'Continue';
 
-Write-Information "Copying $ModuleSourcePath/TypeDefinitions.ps1"
-Copy-Item -Path "$ModuleSourcePath/TypeDefinitions.ps1" -Destination "$ModuleOutPath/" -Force;
+    $ModuleSourcePath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, "./ACME-PS"));
+    $ModuleOutPath = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $ModuleOutPath));
+    $ModuleOutFile = [System.IO.Path]::Combine($ModuleOutPath, "ACME-PS.psm1");
 
-Write-Information "Copying $ModuleSourcePath/Prerequisites.ps1"
-Copy-Item -Path "$ModuleSourcePath/Prerequisites.ps1" -Destination "$ModuleOutPath/" -Force;
+    <# Clean Publish folder #>
+    if(Test-Path $ModuleOutPath) {
+        Write-Information "Delete old files from $ModuleOutPath";
+        Get-ChildItem "$ModuleOutPath/*" -Recurse | Remove-Item -Force -Recurse | Out-Null
+    } else {
+        Write-Information "Creating $ModuleOutPath";
+        New-Item $ModuleOutPath -ItemType Directory
+    }
 
-Write-Information "Copying assets"
-Copy-Item -Path "$ModuleSourcePath/ACME-PS.png" -Destination "$ModuleOutPath/" -Force;
-Copy-Item -Path "$ModuleSourcePath/../LICENSE" -Destination "$ModuleOutPath/" -Force;
+    <# Publish the module #>
+    $ContentFiles = @(
+        "$ModuleSourcePath/ACME-PS.psd1",
+        "$ModuleSourcePath/TypeDefinitions.ps1",
+        "$ModuleSourcePath/Prerequisites.ps1",
+        "$ModuleSourcePath/ACME-PS.png",
+        "$ModuleSourcePath/../LICENSE"
+    )
 
-Import-Module "$PSScriptRoot/ACME-PS" -Force -ErrorAction 'Stop' -Verbose:$false # This will create the All* files.
+    # Class files are sequence sensitive
+    $ClassPath = [System.IO.Path]::Combine($ModuleSourcePath, "./internal/classes");
+    $ClassFiles = @(
+        "crypto/KeyExport",
+        "crypto/KeyBase",
+        "crypto/Certificate",
+        "crypto/RSAKey",
+        "crypto/ECDsaKey",
+        "crypto/KeyAuthorization",
+        "crypto/AlgorithmFactory",
+        "AcmeHttpResponse",
+        "AcmeHttpException",
+        "AcmeDirectory",
+        "AcmeAccount",
+        "AcmeIdentifier",
+        "AcmeChallenge",
+        "AcmeCsrOptions"
+        "AcmeOrder",
+        "AcmeAuthorization",
+        "AcmeState",
+        "AcmeState.InMemory",
+        "AcmeState.DiskPersisted"
+    );
 
-$ModuleFiles = @(
-    "internal/AllClasses.ps1",
-    "internal/AllFunctions.ps1",
-    "AllFunctions.ps1"
-)
+    $ScriptPaths = @(
+        "internal/functions",
+        "functions"
+    )
 
-Write-Information "Merging Module content files $([string]::Join(", ", $ModuleFiles))"
-$ModuleFiles | ForEach-Object { Get-Content "$ModuleSourcePath/$_" } | Set-Content "$ModuleOutPath/ACME-PS.psm1";
+    $ScriptFiles = $ScriptPaths | ForEach-Object { Get-ChildItem -Path ([System.IO.Path]::Combine($ModuleSourcePath, $_)) -Recurse -Include "*.ps1" }
 
-if($SignModule) {
-    Write-Information "Signing all *.ps* files"
-    $files = "$ModuleOutPath/*.ps*"
-    $cert = Get-Item Cert:\CurrentUser\My\39BCA611578AD62BA5126A406DBD4CC5DAFB859C
+    Write-Information "Merge class files into $ModuleOutFile";
+    $ClassFiles | ForEach-Object { [System.IO.Path]::Combine($ClassPath, "$_.ps1") } | Merge-ContentInto -OutFile $ModuleOutFile;
 
-    Set-AuthenticodeSignature -FilePath $files -Certificate $cert | Out-Null
+    Write-Information "Merge srcipt files into $ModuleOutFile";
+    $ScriptFiles | ForEach-Object { $_.FullName } | Merge-ContentInto -OutFile $ModuleOutFile;
+
+    Write-Information "Copy content files";
+    $ContentFiles | ForEach-Object { Copy-Item -Path $_ -Destination "$ModuleOutPath/" }
+
+    if($SignModule) {
+        Write-Information "Signing all *.ps* files"
+        $files = "$ModuleOutPath/*.ps*"
+        $cert = Get-Item Cert:\CurrentUser\My\39BCA611578AD62BA5126A406DBD4CC5DAFB859C
+
+        Set-AuthenticodeSignature -FilePath $files -Certificate $cert | Out-Null
+    }
+
+    Write-Information "Finished building - running tests";
+
+    <# Run tests
+    try {
+        Remove-Module ACME-PS -ErrorAction Ignore
+        Import-Module "$ModuleOutPath\ACME-PS.psd1" -ErrorAction 'Stop'
+
+        Invoke-Pester -Path "$ModuleSourcePath\tests"
+    }
+    catch {
+        Write-Error $error[0];
+    }#>
 }
-
-Write-Information "Finished";
