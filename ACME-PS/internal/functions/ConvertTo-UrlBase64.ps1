601662f592ce7d1f83bb2d2f7c9f04d19b2c0c4e
Thomas Glatzer <glatzert@outlook.de>
2020-05-21 17:30:49 +0200

    Add process block

--- ACME-PS/internal/functions/ConvertTo-UrlBase64.ps1
+++ ACME-PS/internal/functions/ConvertTo-UrlBase64.ps1
@@ -9,15 +9,17 @@ function ConvertTo-UrlBase64 {
         [byte[]] $InputBytes
     )
 
-    if($PSCmdlet.ParameterSetName -eq "FromString") {
-        $InputBytes = [System.Text.Encoding]::UTF8.GetBytes($InputText);
+    process {
+        if($PSCmdlet.ParameterSetName -eq "FromString") {
+            $InputBytes = [System.Text.Encoding]::UTF8.GetBytes($InputText);
+        }
+
+        $encoded = [System.Convert]::ToBase64String($InputBytes);
+
+        $encoded = $encoded.TrimEnd('=');
+        $encoded = $encoded.Replace('+', '-');
+        $encoded = $encoded.Replace('/', '_');
+
+        return $encoded;
     }
-
-    $encoded = [System.Convert]::ToBase64String($InputBytes);
-
-    $encoded = $encoded.TrimEnd('=');
-    $encoded = $encoded.Replace('+', '-');
-    $encoded = $encoded.Replace('/', '_');
-
-    return $encoded;
 }
