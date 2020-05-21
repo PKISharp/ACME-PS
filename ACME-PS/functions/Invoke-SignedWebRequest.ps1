4fc77ba3bc8ad1a779831a8eff582cf4d9dcc625
Thomas Glatzer <glatzert@outlook.de>
2020-05-21 14:12:59 +0200

    Add documentation

--- ACME-PS/functions/Invoke-SignedWebRequest.ps1
+++ ACME-PS/functions/Invoke-SignedWebRequest.ps1
@@ -1,4 +1,37 @@
 function Invoke-SignedWebRequest {
+    <#
+        .SYNOPSIS
+            Sends a POST request to the given URL.
+
+        .DESCRIPTION
+            Sends a POST request to the given URL. It'll use the account, account key and
+            nonce provided in the state object to sign the request and add the anti-replay-nonce.
+            The request will automatically retry, if there's a nonce-error unless indicated otherwise.
+            Generally this CmdLet is used internally only, but it's available publically since, it might be usefull.
+
+
+        .PARAMETER Url
+            The url where the POST or POST-as-GET request should be sent.
+
+        .PARAMETER State
+            The state object, that is used in this module, to provide easy access to the ACME service directory,
+            your account key, the associated account and the replay nonce.
+
+        .PARAMETER Payload
+            The payload of the request. Will be signed if present.
+            Leave empty for POST-as-GET requests.
+
+        .PARAMETER SuppressKeyId
+            Do not include the KeyId parameter in the request.
+
+        .PARAMETER SkipRetryOnNonceError
+            Do not retry the request on nonce-errors.
+
+
+        .EXAMPLE
+            PS (POST-as-GET)> Invoke-SignedWebRequest "https://acme.service/" $myState
+            PS (POST-as-GET)> Invoke-SignedWebRequest -Url "https://acme.service/" -State $myState -Payload $myPayload
+    #>
     [CmdletBinding()]
     [OutputType("AcmeHttpResponse")]
     param(
